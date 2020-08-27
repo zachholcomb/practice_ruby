a27b9b6ba7c40a83391c45fd5f142bc3b78dc340
Zach Holcomb <zachholcombmusic@gmail.com>
2020-08-26 18:07:38 -0600

    Refactor range

--- lib/sum_of_multiples.rb
+++ lib/sum_of_multiples.rb
@@ -5,8 +5,8 @@ class SumOfMultiples
 
   def to(target)
     return 0 if @nums == [0]
-    
-    (0..(target-1)).find_all do |num1|
+
+    (0...target).find_all do |num1|
       @nums.any? { |num2| num1 % num2 == 0 }
     end.sum
   end
